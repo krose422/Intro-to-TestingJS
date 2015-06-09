ea62763e75a526f2d4c863dd164045d7e8c95563
Kelley Rose <krose422@gmail.com>
2015-06-09 17:19:14 -0400

    Finished all but three tests

--- test/test.bar.js
+++ test/test.bar.js
@@ -1,14 +1,62 @@
-/* global describe, it */
 
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-        expect(1).to.equal(1);
+  describe("Bar", function() {
+  var bar;
+
+  beforeEach(function() {
+    bar = new Bar();
+  });
+
+    describe('squared', function () {
+      it('should return the square of a number', function () {
+        expect( bar.squared(2) ).to.equal(4);
       });
     });
-  });
+
+    describe('cubed', function () {
+      it('should return the cube of a number', function () {
+        expect( bar.cubed(2) ).to.equal(8);
+      });
+    });
+
+    // KELLEY NEED TO FIX THIS ONE
+    describe('exponentOf', function () {
+      it('should return the exponent of a number', function () {
+        expect( bar.exponentOf(2) ).to.equal();
+      });
+    });
+
+    describe('reverseString', function () {
+      it('should return the reverse of a string', function () {
+        expect( bar.reverseString("kelley") ).to.equal("yellek");
+      });
+    });
+
+    // KELLEY FIX
+    describe('cipher', function () {
+      it('', function () {
+        expect( bar.cipher("kelley") ).to.equal("");
+      });
+    });
+
+    // KELLEY FIX
+    describe('decipher', function () {
+      it('', function () {
+        expect( bar.decipher("kelley") ).to.equal("");
+      });
+    });
+
+    describe('rot13', function () {
+      it('should change sentence into rot13 code', function () {
+        expect( bar.rot13("i like cheese") ).to.equal("v yvxr purrfr");
+      });
+    });
+
+
+
+  })
+
 
 })();
